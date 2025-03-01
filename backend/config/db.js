ad75ec37131a043f74d7a593fef40afb52d5803a
duy99uit <58467999+duy99uit@users.noreply.github.com>
2025-03-01 11:51:49 +0700

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://ntp:thanhphat1510@cluster0.q91pm.mongodb.net/Phone_Laptop?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://ntp:thanhphat@cluster0.q91pm.mongodb.net/Phone_Laptop?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => console.log("DB Connected to NTP"));
 };
